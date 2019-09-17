da6604cc24fd23599c796b1895bfce46abb4cde2
Wilder Medeiros <wilder.medeiros@outlook.com>
2019-09-17 02:15:47 -0300

    Initial commit

---
+++ Assets/SpaceShip.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip : MonoBehaviour
+{
+	Rigidbody rigidBody;
+
+	[SerializeField ]float speed = 20f;
+
+	void Start()
+    {
+		rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+	{
+		ProcessInput();
+	}
+
+	private void ProcessInput()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			Debug.Log("Space pressed");
+		}
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			Debug.Log("Rotating Left");
+		}
+		else if (Input.GetKey(KeyCode.D))
+		{
+			Debug.Log("Rotating Right");
+		}
+	}
+}
