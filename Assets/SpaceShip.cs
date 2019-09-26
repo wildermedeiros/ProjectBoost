280c6de5238d258d848788e6e9c1e8e3f3a49001
Wilder Medeiros <wilder.medeiros@outlook.com>
2019-09-26 03:09:40 -0300

    Tuning Up the design

--- Assets/SpaceShip.cs
+++ Assets/SpaceShip.cs
@@ -7,8 +7,8 @@ public class SpaceShip : MonoBehaviour
 	Rigidbody rigidBody;
 	AudioSource audioSource;
 
-	[SerializeField] float mainThruster = 5f;
-	[SerializeField] float rotationThruster = 5f;
+	[SerializeField] float mainThrust = 100;
+	[SerializeField] float rcsThrust = 100;
 
 	void Start()
     {
@@ -18,30 +18,57 @@ public class SpaceShip : MonoBehaviour
 
     void Update()
 	{
-		ProcessInput();
+		Thrust();
+		Rotate();
 	}
 
-	private void ProcessInput()
+	private void OnCollisionEnter(Collision collision)
 	{
+		if (collision.gameObject.CompareTag("Friendly"))
+		{
+			Debug.Log("Friendly");
+		} else if (collision.gameObject.CompareTag("Finish"))
+		{
+			Debug.Log("Finish");
+		} else
+		{
+			Debug.Log("Dead");
+			// kill de player
+		}
+	}
+
+	private void Thrust()
+	{
+
+		float thrustThisFrame = mainThrust * Time.deltaTime;
 		if (Input.GetKey(KeyCode.Space))
 		{
-			rigidBody.AddRelativeForce(Vector3.up * mainThruster * Time.deltaTime);
+			rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
 			if (!audioSource.isPlaying)
 			{
 				audioSource.Play();
 			}
-		} else
+		}
+		else
 		{
 			audioSource.Stop();
 		}
+	}
 
+	private void Rotate()
+	{
+		rigidBody.freezeRotation = true;
+
+		float rotationThisFrame = rcsThrust * Time.deltaTime;
 		if (Input.GetKey(KeyCode.A))
 		{
-			transform.Rotate(Vector3.forward * rotationThruster * Time.deltaTime);
+			transform.Rotate(Vector3.forward * rotationThisFrame);
 		}
 		else if (Input.GetKey(KeyCode.D))
 		{
-			transform.Rotate(-Vector3.forward * rotationThruster * Time.deltaTime);
+			transform.Rotate(-Vector3.forward * rotationThisFrame);
 		}
+
+		rigidBody.freezeRotation = false; 
 	}
 }
